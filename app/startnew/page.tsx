1aec1f32ab77958eee552e04ff653c665d23d1e0
raj-saroj-vst-au4 <raj.s.saroj@gmail.com>
2023-10-22 18:50:09 +0530

    alpha release v1.1

--- app/startnew/page.tsx
+++ app/startnew/page.tsx
@@ -64,9 +64,7 @@ function StartSessModal() {
       <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
         <div className="flex flex-col justify-between">
           <div className="space-y-2">
-            <h2 className="text-4xl font-bold leadi lg:text-5xl">
-              Let's talk!
-            </h2>
+            <h2 className="text-4xl font-bold leadi lg:text-5xl">Lets talk!</h2>
             <div className="dark:text-gray-400">Start New Q&A Session</div>
           </div>
           <img src="/start.svg" alt="" className="p-6 h-100" />
